ccbc674da980d805e5888bf82b438a04e85a25ea
diegodimer <diego.dimer@ufrgs.br>
2019-11-24 23:57:22 -0300

    2f começada

--- planopt_heuristics/pattern_hillclimbing.cc
+++ planopt_heuristics/pattern_hillclimbing.cc
@@ -8,9 +8,11 @@
 
 using namespace std;
 
-namespace planopt_heuristics {
-vector<set<int>> compute_causally_relevant_variables(const TNFTask &task) {
-    /*
+namespace planopt_heuristics
+{
+vector<set<int>> compute_causally_relevant_variables(const TNFTask &task)
+{
+  /*
         v is causally relevant for w
       iff
         v is a predecessor of w in the causal graph, or
@@ -25,58 +27,81 @@ vector<set<int>> compute_causally_relevant_variables(const TNFTask &task) {
         v and w occur in the same operator (and at least one of them changes the state).
     */
 
-    int num_variables = task.variable_domains.size();
-    vector<set<int>> relevant(num_variables);
-    for (const TNFOperator &op : task.operators) {
-        for (const TNFOperatorEntry &e1 : op.entries) {
-            for (const TNFOperatorEntry &e2 : op.entries) {
-                if (e1.variable_id != e2.variable_id &&
-                        (e1.precondition_value != e1.effect_value ||
-                         e2.precondition_value != e2.effect_value)) {
-                    relevant[e1.variable_id].insert(e2.variable_id);
-                }
-            }
+  int num_variables = task.variable_domains.size();
+  vector<set<int>> relevant(num_variables);
+  for (const TNFOperator &op : task.operators)
+  {
+    for (const TNFOperatorEntry &e1 : op.entries)
+    {
+      for (const TNFOperatorEntry &e2 : op.entries)
+      {
+        if (e1.variable_id != e2.variable_id &&
+            (e1.precondition_value != e1.effect_value ||
+             e2.precondition_value != e2.effect_value))
+        {
+          relevant[e1.variable_id].insert(e2.variable_id);
         }
+      }
     }
-    return relevant;
+  }
+  return relevant;
 }
 
-bool HillClimber::fits_size_bound(const std::vector<Pattern> &collection) const {
-    /*
+bool HillClimber::fits_size_bound(const std::vector<Pattern> &collection) const
+{
+  /*
       Compute the number of abstract states in the given collection without
       explicitly computing the projections. Return true if the total size
       is below size_bound and false otherwise.
     */
+  // exercicio (f)
+  // calcular a soma dos estados abstratos: tem um total do conjunto, e um numero de estados pra cada pattern no conjunto
+  int total = 0;
+  for (const auto p : collection)
+  { // pra cada pattern na coleção
+    int val = 1;
+    for (const auto e : p)
+    {                                  // pra cada pattern dentro da coleção
+      val *= task.variable_domains[e]; // multiplica pelo dominio da variável no pattern
+    }
+    total += val;           // soma no total do conjunto
+    if (total > size_bound) // se passou do bound retorna falso
+      return false;
+  }
 
-
-    // TODO: add your code for exercise (f) here.
-
-    return true;
+  return true;
 }
-
 
 HillClimber::HillClimber(const TNFTask &task, int size_bound, vector<TNFState> &&samples)
     : task(task),
       size_bound(size_bound),
       samples(move(samples)),
-      causally_relevant_variables(compute_causally_relevant_variables(task)){
+      causally_relevant_variables(compute_causally_relevant_variables(task))
+{
 }
 
-
-vector<Pattern> HillClimber::compute_initial_collection() {
-    /*
+vector<Pattern> HillClimber::compute_initial_collection()
+{
+  /*
       In TNF, every variable occurs in the goal state, so we create the
       collection {{v} | v \in V}.
     */
-    vector<Pattern> collection;
 
-    // TODO: add your code for exercise (f) here.
+  vector<Pattern> collection;
+  // exercício (f)
+  for (const auto v : task.goal_state)
+  { // pra cada variável citada no goal eu ponho na coleção
+    Pattern p;
+    p.push_back(v);
+    collection.push_back(p);
+  }
 
-    return collection;
+  return collection;
 }
 
-vector<vector<Pattern>> HillClimber::compute_neighbors(const vector<Pattern> &collection) {
-    /*
+vector<vector<Pattern>> HillClimber::compute_neighbors(const vector<Pattern> &collection)
+{
+  /*
       for each pattern P in the collection C:
           compute the set of variables that are causally relevant for any V in P
              (Use the precomputed information in causally_relevant_variables.)
@@ -85,28 +110,55 @@ vector<vector<Pattern>> HillClimber::compute_neighbors(const vector<Pattern> &co
               add the collection C' := C u {P u {V}} to neighbors
 
     */
-    vector<vector<Pattern>> neighbors;
+  vector<vector<Pattern>> neighbors;
 
-    // TODO: add your code for exercise (f) here.
+  // exercício (f)
 
-    return neighbors;
-}
-
-vector<int> HillClimber::compute_sample_heuristics(const vector<Pattern> &collection) {
-    CanonicalPatternDatabases cpdbs(task, collection);
-    vector<int> values;
-    values.reserve(samples.size());
-    for (const TNFState &sample : samples) {
-        values.push_back(cpdbs.compute_heuristic(sample));
+  // pra cada P na coleção C:
+  for(auto p: collection){
+    // computa o conjunto de variaveis casualmente relevantes pra qualquer variavel no patter n
+    set<int> causally_relevant;
+    for(auto v:p){ // causally_relevant tem todas variaveis q sao casualmente relevantes pra qualquer variavel no patter
+      for( auto x : causally_relevant_variables[v] ) {
+        causally_relevant.insert(x);
+      }
     }
-    return values;
+    for(auto v : p){ // tira do set todas as variáveis que já ocorrem em p
+      if(causally_relevant.find(v) != causally_relevant.end()){
+        causally_relevant.erase(v);
+      }
+    }
+
+    // com as variaveis que sobrou faz uma collection e poem nos vizinhos
+    vector<Pattern> clinha = collection; // coleção clinha é C
+    for(auto v : causally_relevant){
+      p.push_back(v); // P união com V
+    }
+    clinha.push_back(p); // união com C
+    neighbors.push_back(clinha); // adiciona aos vizinhos
+  }
+
+  return neighbors;
 }
 
-vector<Pattern> HillClimber::run() {
-    vector<Pattern> current_collection = compute_initial_collection();
-    vector<int> current_sample_values = compute_sample_heuristics(current_collection);
+vector<int> HillClimber::compute_sample_heuristics(const vector<Pattern> &collection)
+{
+  CanonicalPatternDatabases cpdbs(task, collection);
+  vector<int> values;
+  values.reserve(samples.size());
+  for (const TNFState &sample : samples)
+  {
+    values.push_back(cpdbs.compute_heuristic(sample));
+  }
+  return values;
+}
 
-    /*
+vector<Pattern> HillClimber::run()
+{
+  vector<Pattern> current_collection = compute_initial_collection();
+  vector<int> current_sample_values = compute_sample_heuristics(current_collection);
+
+  /*
       current := an initial candidate
       loop forever:
           next := a neighbor of current with maximal improvement over current
@@ -121,8 +173,8 @@ vector<Pattern> HillClimber::run() {
       modify current_collection.
     */
 
-
-    // TODO: add your code for exercise (f) here.
-    return current_collection;
+  // TODO: add your code for exercise (f) here.
+  cout<<"AAAAAA"<<endl;
+  return current_collection;
 }
-}
+} // namespace planopt_heuristics
